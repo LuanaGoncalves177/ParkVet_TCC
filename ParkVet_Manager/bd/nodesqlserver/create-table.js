29e8ee2e45a50032adba7610ec62d52b11c3f997
LuanaGoncalves177 <76621475+LuanaGoncalves177@users.noreply.github.com>
2021-11-15 21:57:53 -0300

    Reestruturação do trabalho e redefinição do main

--- ParkVet_Manager/bd/nodesqlserver/create-table.js
+++
@@ -1,7 +0,0 @@
-
-const connStr = "Server=XXX;Database=XXX;User Id=XXX;Password=XXX;";
-const sql = require("mssql");
-
-sql.connect(connStr)
-   .then(conn => console.log("conectou!"))
-   .catch(err => console.log("erro! " + err));
