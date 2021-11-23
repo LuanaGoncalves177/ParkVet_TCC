b758d5de29cf7f0cdf3042a8544f45fdc992d08f
FrancianeF <92341189+FrancianeF@users.noreply.github.com>
2021-11-23 16:02:47 -0300

    Filtro de agendados

--- ParkVet_Manager/js/3-AgendamentosJS/agendamentos-agendados.js
+++ ParkVet_Manager/js/3-AgendamentosJS/agendamentos-agendados.js
@@ -1,29 +1,37 @@
-var campoFiltro = document.querySelector("#data-servico");
+var botaoFiltro = document.querySelector("#botao-filtrar");
+var imgExcluir = document.querySelector("#agenda")
 
-campoFiltro.addEventListener("input", function(event){
-   
-    //var dataEscolhida = this.value;
-    var clientesAgendados = document.querySelectorAll(".clientes-agendados");
+botaoFiltro.addEventListener("click", function(event){
+    event.preventDefault()
+    var dataEscolhidaOriginal = document.querySelector("#data_servico").value;
+    console.log(dataEscolhidaOriginal)
+    var dataEscolhida = dataEscolhidaOriginal.split('-').reverse().join('/');
+    console.log(dataEscolhida)
     
-    if (this.value.length != 0){
-        for(var i=0; i < clientesAgendados.length; i++){
-
+    var clientesAgendados = document.querySelectorAll(".clientes-agendados");
+    console.log(clientesAgendados.length)
+    for(var i=0; i < clientesAgendados.length; i++){
+            
             var clienteAgendado = clientesAgendados[i];
-            var tdData = clienteAgendado.querySelector(".info-data");
+            //console.log(clienteAgendado)
+            var tdData = clienteAgendado.querySelector(".agenda__data");
+            //console.log(tdData)
             var dataAgendada = tdData.textContent;
-            var expressao = new RegExp(this.value, "i")
+            var expressao = new RegExp(dataEscolhida, "i")
             
             if(!expressao.test(dataAgendada)){
                 clienteAgendado.classList.add("invisivel");
             }else{
                 clienteAgendado.classList.remove("invisivel");
             }
-        }
-    }else{
-        for (var i = 0; i < clientesAgendados.length; i++){
-            var clienteAgendado =  clientesAgendados[i];
-           clienteAgendado.classList.remove("invisivel");
-        }
     }
 
 });
+
+imgExcluir.addEventListener("click", function(event){
+    event.target.parentNode.classList.add("fadeOut")
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+
+})
