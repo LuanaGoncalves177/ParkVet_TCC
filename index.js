706886bdc01d4ccfbeacd59e5ee4897b7727d374
FrancianeF <92341189+FrancianeF@users.noreply.github.com>
2021-11-24 15:13:23 -0300

    Site no Servidor

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const express = require('express')
 const app = express()
 
 app.listen(4000,() => console.log("Deu Certo"))
+app.use(express.static(__dirname + "/ParkVet_Manager"));
+app.get('/', (req, res) => 
+    res.sendFile(__dirname + "/ParkVet_Manager/index.html"))
 
-app.get('/', (req, res) => res.send("Servidor Rodando"))
