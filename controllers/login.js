e6d5cfa7e76fb13236cf972f0a24470c4e48c8f0
FrancianeF <92341189+FrancianeF@users.noreply.github.com>
2021-11-25 18:29:41 -0300

    Servidor

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,10 @@
+const express = require('express')
+
 module.exports = app => {
-    app.use(express.static(__dirname + "/ParkVet_Manager"));
-    app.get('/', (req, res) => res.sendFile("D:/GitHub/ParkVet_TCC/ParkVet_Manager"))
-    //app.get('/', (req, res) => res.send("Deu certo"))
+    app.use(express.static("D:/GitHub/ParkVet_TCC/ParkVet_Manager"));
+    
+    app.get('/', (req, res) => res.sendFile(__dirname + "/ParkVet_TCC/ParkVet_Manager/index.html"))
+
+    app.get('/manager.html', (req,res) => res.sendFile(__dirname + "/ParkVet_TCC/ParkVet_Manager/manager.html"))
+    
 }
