2e0ee2688cea0ed485b285f10fc7d078dc2bd883
FrancianeF <92341189+FrancianeF@users.noreply.github.com>
2021-11-27 00:30:01 -0300

    Tentativa de Arrumar o Server

--- controllers/login.js
+++ controllers/login.js
@@ -1,13 +1,18 @@
 const express = require('express')
 
+
 module.exports = app => {
-    app.use(express.static("ParkVet_Manager"));
+
+    //app.use(express.static("ParkVet_Manager"));
     
-    app.get('/', (req, res) => res.sendFile(__dirname + "/ParkVet_Manager/index.html"))
+    app.get('/', (req, res) => {
+        res.sendFile("D:/GitHub/ParkVet_TCC/ParkVet_Manager/index.html")
+        //res.send("Esta no inicio")
+    })
 
     app.get('/manager.html', (req,res) => {
         //res.sendFile(__dirname + "/ParkVet_TCC/ParkVet_Manager/manager.html")
-        console.log("Manager")
+        
 })
     
 }
