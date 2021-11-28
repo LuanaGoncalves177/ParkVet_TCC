fd3785630c0f0e4e4ca658d0a10ac5ad58f5db3b
FrancianeF <92341189+FrancianeF@users.noreply.github.com>
2021-11-28 13:25:05 -0300

    Alterações e criação de rotas no servidor

--- controllers/login.js
+++ controllers/login.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const body = require('body-parser')
 const session = require('express-session')
+const router = require('express').Router
 
 
 module.exports = app => {
@@ -10,28 +11,53 @@ module.exports = app => {
         secret: "cookie_secret",
         resave: true,
         saveUninitialized:true}));
-    app.engine('html', require ('ejs').renderFile);
-    app.set('view engine', 'html');
-    app.use('/ParkVet_Manager', express.static("ParkVet_Manager"));
-    app.set('ParkVet_Manager', path.join(__dirname, '/ParkVet_Manager'))
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine','html')
+    app.use('/Public', express.static(path.join(__dirname, 'Public')))
+    app.set('index', path.join(__dirname,'/Views'))
 
     var login = "001"
     var password = "12345"
     
-    app.post('/manager', (req, res) => {
-        if(req.body.senha == password && req.body.id == login){
+    //app.get('/', (req, res)=> {
+    //    res.render('index')
+    //})
+
+    //fazer o redirecionamento para /manager ou /administrador 
+//-----------------------------
+function Rota(){
+    router.get('/manager', (req, res) =>{
+        if(req.body.senha == password && req.body.login == login){
+            //app.use(passport.session())
+            
             req.session.login = login
         }
-        res.render("manager")
+        res.render("cadastro")
+    } )
+    router.use('/cadastro')
+
+}
+
+//----------------------------
+    app.get('/manager', (req, res)=> {
+        res.render('manager')
     })
 
-    app.get('/',(req, res)=> {
+    app.post('/manager', Rota())
+
+    app.get('/manager', (req, res)=> {
         if(req.session.login){
-            res.render('./1-Cadastro/cadastro')
+            res.render("cadastro")
         }else{
             res.render('manager')
         }
+
+    //app.get('/manager/cadastro', (req, res)=>{
+      //  res.render(cadastro)
+    //})
+    
     })      
+
 }
     
 
