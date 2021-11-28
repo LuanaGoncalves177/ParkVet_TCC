2244b75d76282c646cbd3b5f00c49f0416e9c2b6
FrancianeF <92341189+FrancianeF@users.noreply.github.com>
2021-11-28 17:03:17 -0300

    Ateração de Arquivos

--- controllers/login.js
+++ controllers/login.js
@@ -4,54 +4,30 @@ const body = require('body-parser')
 const session = require('express-session')
 
 
+let rotasLoginFuncionario = require('../rotasLoginFuncionario')
+let rotaCadastramento = require('../rotaCadastramento')
+
 module.exports = app => {
     app.use(body.urlencoded({extended:true}));
+    
     app.use(session({
         secret: "cookie_secret",
         resave: true,
         saveUninitialized:true}));
-    
+
     app.engine('html', require('ejs').renderFile);
     app.set('view engine','html')
     app.use('/Public', express.static(path.join(__dirname, 'Public')))
-    app.set('index', path.join(__dirname,'/Views'))
-
-    var login = "001"
-    var password = "12345"
+    app.set(path.join(__dirname,'/Views'))
     
-    //app.get('/', (req, res)=> {
-    //    res.render('index')
-    //})
 
-    //fazer o redirecionamento para /manager ou /administrador 
-
-    app.get('/manager', (req, res)=> {
-        res.render('manager')
+    app.get('/', (req, res)=>{
+        res.render('index')
     })
 
-    app.post('/manager', (req, res) => {
-        if(req.body.senha == password && req.body.login == login){
-            //app.use(passport.session())
-            
-            req.session.login = login
-        }
-        res.render("cadastro")
-    })
+    app.use('/manager', rotasLoginFuncionario)
+    app.use('/cadastro', rotaCadastramento)
 
-    app.get('/manager/:id',(req, res)=> {
-        if(req.session.login){
-            var userRoute = require('/user/:id')
-            app.use('/manager', userRoute)
-            res.render("cadastro")
-        }else{
-            res.render('manager')
-        }
-
-    //app.get('/manager/cadastro', (req, res)=>{
-      //  res.render(cadastro)
-    //})
-    
-    })      
 
 }
     
